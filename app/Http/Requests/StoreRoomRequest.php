3e681b928335f471e80eebffd20fccdacc30235a
Andrés Yáñez <camilo_escobar2398@outlook.com>
2024-11-05 22:34:57 -0500

    Agregando la validación de las condiciones al registrar y actualizar una Habitación.

--- app/Http/Requests/StoreRoomRequest.php
+++ app/Http/Requests/StoreRoomRequest.php
@@ -31,4 +31,27 @@ class StoreRoomRequest extends FormRequest
             RoomEnum::CountRooms => ['required', 'integer', 'min:1'],
         ];
     }
+
+    /**
+     * Validate conditions for Room
+     */
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+            $roomTypeId = $this->input(RoomEnum::RoomTypeId);
+            $accommodationId = $this->input(RoomEnum::RoomAccommodationId);
+
+            if ($roomTypeId == RoomTypeEnum::StandarId && !in_array($accommodationId, [RoomAccommodationEnum::SimpleId, RoomAccommodationEnum::DoubleId])) {
+                $validator->errors()->add('room_accommodation', 'Para el tipo de habitación Estándar, la acomodación debe ser Sencilla o Doble.');
+            }
+
+            if ($roomTypeId == RoomTypeEnum::JuniorId && !in_array($accommodationId, [RoomAccommodationEnum::TripleId, RoomAccommodationEnum::QuadrupleId])) {
+                $validator->errors()->add('room_accommodation', 'Para el tipo de habitación Junior, la acomodación debe ser Triple o Cuádruple.');
+            }
+
+            if ($roomTypeId == RoomTypeEnum::SuiteId && !in_array($accommodationId, [RoomAccommodationEnum::SimpleId, RoomAccommodationEnum::DoubleId, RoomAccommodationEnum::TripleId])) {
+                $validator->errors()->add('room_accommodation', 'Para el tipo de habitación Suite, la acomodación debe ser Sencilla, Doble o Triple.');
+            }
+        });
+    }
 }
