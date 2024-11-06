f95a54e4dbb72d3825b013a910d648ebfea23d4c
Andrés Yáñez <camilo_escobar2398@outlook.com>
2024-11-05 20:26:58 -0500

    Creando el AbstractRepository y el HotelRepository

---
+++ app/Repositories/AbstractRepository.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Collection;
+
+abstract class AbstractRepository
+{
+    /**
+     * @var Model
+     */
+    protected $model;
+
+    /**
+     * AbstractRepository constructor.
+     *
+     * @param Model $model
+     */
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * Get all records.
+     *
+     * @param array $columns
+     * @return Collection
+     */
+    public function all(array $columns = ['*']): Collection
+    {
+        return $this->model->all($columns);
+    }
+
+    /**
+     * Find a record by ID.
+     *
+     * @param int $id
+     * @param array $columns
+     * @return Model|null
+     */
+    public function find(int $id, array $columns = ['*']): ?Model
+    {
+        return $this->model->find($id, $columns);
+    }
+
+    /**
+     * Create a new record.
+     *
+     * @param array $data
+     * @return Model
+     */
+    public function create(array $data): Model
+    {
+        return $this->model->create($data);
+    }
+
+    /**
+     * Update a record by ID.
+     *
+     * @param int $id
+     * @param array $data
+     * @return bool
+     */
+    public function update(int $id, array $data): bool
+    {
+        $record = $this->find($id);
+        return $record ? $record->update($data) : false;
+    }
+
+    /**
+     * Delete a record by ID.
+     *
+     * @param int $id
+     * @return bool
+     */
+    public function delete(int $id): bool
+    {
+        $record = $this->find($id);
+        return $record ? $record->delete() : false;
+    }
+}
